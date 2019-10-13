b3a0a3ddb124fcfa5d2ec789ca83f1e4992acdf7
s-leirbag <gabriel@shiu.us>
2019-10-13 15:35:32 -0400

    basic banking and buyor pass property prompts

--- Player.gd
+++ Player.gd
@@ -1,6 +1,11 @@
 extends PathFollow
 
 var is_moving = false
+onready var banker = get_node("root/Board/Banker")
+
+func _ready():
+	$Finances.connect("pay_rent", banker, "_on_player_pay_rent")
+	$Finances.connect("buy_property", banker, "_on_player_buy_property")
 
 func _process(delta):
 	if is_moving:
