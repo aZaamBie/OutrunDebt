aeb421e22bc7bfeddf01bd23f50c023d1f1fbe09
m-mood <52883623+m-mood@users.noreply.github.com>
2019-10-13 21:37:51 +0200

    Add yield

--- Player.gd
+++ Player.gd
@@ -1,12 +1,18 @@
 extends PathFollow
 
-var is_moving = false
+var is_moving = true
 
 func _process(delta):
+	# Moves player along the path
 	if is_moving:
 		set_offset(offset + 0.1)
 		print("player path offset: ", get_offset())
+	else:
+		yield(self, "move_ready")
 
 func _input(event):
 	if event is InputEventMouseButton:
 		is_moving = !is_moving
+
+func _ready():
+	set_offset(offset + 0.1)
