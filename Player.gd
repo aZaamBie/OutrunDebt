41f493b9179adf5897449492f6f9d97899f5bbe3
m-mood <52883623+m-mood@users.noreply.github.com>
2019-10-13 21:57:38 +0200

    Fixed bug
    2 _ready functions in same class

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ onready var banker = get_node("root/Board/Banker")
 func _ready():
 	$Finances.connect("pay_rent", banker, "_on_player_pay_rent")
 	$Finances.connect("buy_property", banker, "_on_player_buy_property")
+	set_offset(offset + 0.1)
 
 func _process(delta):
 	# Moves player along the path
@@ -18,6 +19,3 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton:
 		is_moving = !is_moving
-
-func _ready():
-	set_offset(offset + 0.1)
