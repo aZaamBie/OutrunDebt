316add79095c151d88b59c31d31983c678f26f01
s-leirbag <gabriel@shiu.us>
2019-10-13 19:09:26 -0400

    better turns, still glitchy

--- Player.gd
+++ Player.gd
@@ -1,16 +1,18 @@
 extends PathFollow
 
-export var SPEED = 20
-var target_offset = 0
+export var SPEED = 5
+var is_moving = false
+var target_offset = BoardPlaces.boardplaces[1]
 onready var banker = get_node("root/Board/Banker")
 
 func _ready():
+	set_offset(BoardPlaces.boardplaces[0])
 	$Finances.connect("pay_rent", banker, "_on_player_pay_rent")
 	$Finances.connect("buy_property", banker, "_on_player_buy_property")
 
 func _process(delta):
-	if target_offset > offset:
-		set_offset(max(offset + SPEED * delta, target_offset))
-	elif target_offset < offset:
-		set_offset(offset + SPEED * delta)
- 
+	if is_moving:
+		if target_offset > offset:
+			set_offset(max(offset + SPEED * delta, target_offset))
+		elif target_offset < offset:
+			set_offset(offset + SPEED * delta)
--- TurnManager.gd
+++ TurnManager.gd
@@ -6,17 +6,22 @@ onready var path = get_node("/root/Board/City/Path")
 
 func _ready():
 	current_player = path.get_node("Player1")
+	current_player.is_moving = true
 	current_player_num = 1
 
 func _process(delta):
-	if current_player.offset == current_player.target_offset:
+	if int(current_player.offset) == int(current_player.target_offset):
 		current_player.get_node("Camera").set_current(false)
+		current_player.is_moving = false
 		current_player_num += 1
 		if current_player_num > 4:
 			current_player_num = 1
 		current_player = path.get_node("Player" + str(current_player_num))
 		
 		var steps = randi() % 6 + 1
-		var index = BoardPlaces.boardplaces.find(current_player.offset) + steps
+		print(BoardPlaces.boardplaces.find(current_player.target_offset))
+		var index = BoardPlaces.boardplaces.find(current_player.target_offset) + steps
+		print(index)
 		current_player.target_offset = BoardPlaces.boardplaces[index]
 		current_player.get_node("Camera").set_current(true)
+		current_player.is_moving = true
